4831ee84241230d9b458315acfee5fe8f96ad3cc
Sean Behan <codebam@riseup.net>
2025-05-20 17:06:36 -0400

    try fix

--- nixos/modules/theme.nix
+++ nixos/modules/theme.nix
@@ -3,6 +3,10 @@
 {
   stylix = {
     enable = true;
+    image = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/Sahil-958/walls/refs/heads/main/hyprdots/Catppuccin-Mocha/cat_leaves.png";
+      sha256 = "1894y61nx3p970qzxmqjvslaalbl2skj5sgzvk38xd4qmlmi9s4i";
+    };
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     fonts = {
