b458eda218e6280d6a54ab40ac64c80526068e3e
makano <makanobush@gmail.com>
2025-07-11 18:46:43 +0300

    pulseaudio network

--- nixos/modules/hardware.nix
+++ nixos/modules/hardware.nix
@@ -21,6 +21,16 @@
       support32Bit = true;
     };
     pulse.enable = true;
+    extraConfig.pipewire-pulse = {
+    "99-network-tcp" = {
+      "pulse.cmd" = [
+          {
+            cmd = "load-module";
+            args = "module-native-protocol-tcp auth-ip-acl=192.168.100.0/24 auth-anonymous=1";
+          }
+        ];
+      };
+    };    
     jack.enable = true;
     wireplumber.enable = true;
   };
